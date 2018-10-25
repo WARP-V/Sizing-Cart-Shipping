dc0040def2d01e0178b71fb1ac9b41e7ea28279b
pouyaghazvini <pouyaghazvini@gmail.com>
2018-10-25 14:04:12 -0700

    add bodyparser to express middleware

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
-
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '../client/dist'));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World');
