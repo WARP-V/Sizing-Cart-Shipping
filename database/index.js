df85b695a576ac070bb4813bafad098cc44a5cef
pouyaghazvini <pouyaghazvini@gmail.com>
2018-10-26 10:18:36 -0700

    refactored index.js

--- database/index.js
+++ database/index.js
@@ -1,36 +1,11 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/jordandb');
-let db = mongoose.connection;
-const Schema = mongoose.Schema;
+let mongooseURI = mongoose.connect('mongodb://localhost/jordandb');
+let db = mongoose.connect(mongooseURI);
+
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log("connection to db has been made");
 })
 
-let sizingSchema = new Schema({
-  shoeId: {type: Number, unique: true},
-  sizes: [Number]
-});
-
-let descriptionSchema = new Schema({
-  shoeId: {type: Number, unique: true},
-  description: String,
-  style: Number,
-  shown: String,
-});
-
-var Size = mongoose.model('Size', sizingSchema);
-var Description = mongoose.model('Description', descriptionSchema);
-
-var newSize = new Size(sizeData);
-newSize.insertMany(function(err) {
-  if(err) {
-    return console.log(err);
-  } else {
-    
-  }
-})
-
-module.exports.Size = Size;
-module.exports.Description = Description;
+module.exports.db = db;
