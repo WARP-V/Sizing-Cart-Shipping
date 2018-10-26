4240606868b5f0dde581b061c155140620701380
pouyaghazvini <pouyaghazvini@gmail.com>
2018-10-25 18:52:43 -0700

    changed port number

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3003;
 const bodyParser = require('body-parser');
+
 
 app.use(express.static(__dirname + '../client/dist'));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World');
-});
+}); 
 
 app.listen(port, () => console.log(`app is listening on port ${port}`));
