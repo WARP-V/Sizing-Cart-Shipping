7c36a55133efb23bf8c6107c6e953a6526d8a6e7
pouyaghazvini <pouyaghazvini@gmail.com>
2018-10-31 18:31:06 -0700

    started implementing css

--- client/src/components/Sizes.jsx
+++ client/src/components/Sizes.jsx
@@ -6,7 +6,7 @@ class Sizes extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      row: []
     }
   }
   
@@ -15,8 +15,9 @@ class Sizes extends React.Component {
     console.log(this.props);
     return (
      <div className="sizes-table">
-      {this.props.sizes.map(function(size){
-        return <button>{size}</button>
+      {this.props.sizes.map(function(size) {
+        let row = <div></div>
+        return <button className='sizes-buttons'>{size}</button>
       })}
      </div> 
     )
