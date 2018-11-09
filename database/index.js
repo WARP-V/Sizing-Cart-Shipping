a2c6e2804a7e09dfa09c28c2101502918cac7846
pouyaghazvini <pouyaghazvini@gmail.com>
2018-11-09 10:24:07 -0800

    dockerized module

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoUri = 'mongodb://localhost:27017/airjordan';
+const mongoUri = 'mongodb://mongo:27017/airjordan';
 const db = mongoose.connect(mongoUri, { useNewUrlParser: true });
 mongoose.set('useCreateIndex', true);
 
