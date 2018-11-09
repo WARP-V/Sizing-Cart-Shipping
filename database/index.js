7d30c2484ce8980dee7ad81913fe70bad03dfdb4
pouyaghazvini <pouyaghazvini@gmail.com>
2018-11-09 10:49:19 -0800

    created new docker container

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoUri = 'mongodb://mongo:27017/airjordan';
+const mongoUri = 'mongodb://172.17.0.2/airjordan';
 const db = mongoose.connect(mongoUri, { useNewUrlParser: true });
 mongoose.set('useCreateIndex', true);
 
